2db27f03cc9a36f07cf21a6d1afe8ae61ef820bb
prabhjyot009 <prabh156dps@gmail.com>
2023-04-04 13:25:54 +0530

    commit

--- tests/students_test.py
+++ tests/students_test.py
@@ -42,6 +42,23 @@ def test_post_assignment_student_1(client, h_student_1):
     assert data['teacher_id'] is None
 
 
+def test_post_assignment_update_student_1(client, h_student_1):
+    content = 'TEST UPDATEPOST'
+
+    response = client.post(
+        '/student/assignments',
+        headers=h_student_1,
+        json={
+            'id': 6,
+            'content': content
+        })
+
+    assert response.status_code == 200
+
+    data = response.json['data']
+    assert data['content'] == content
+
+
 def test_submit_assignment_student_1(client, h_student_1):
     response = client.post(
         '/student/assignments/submit',
--- tests/teachers_test.py
+++ tests/teachers_test.py
@@ -89,8 +89,7 @@ def test_grade_assignment_draft_assignment(client, h_teacher_1):
     """
     response = client.post(
         '/teacher/assignments/grade',
-        headers=h_teacher_1
-        , json={
+        headers=h_teacher_1, json={
             "id": 2,
             "grade": "A"
         }
@@ -100,3 +99,20 @@ def test_grade_assignment_draft_assignment(client, h_teacher_1):
     data = response.json
 
     assert data['error'] == 'FyleError'
+
+
+def test_grade_assignment_teacher_1(client, h_teacher_1):
+    response = client.post(
+        '/teacher/assignments/grade',
+        headers=h_teacher_1,
+        json={
+            "id": 1,
+            "grade": "A"
+        }
+    )
+
+    assert response.status_code == 200
+
+    data = response.json['data']
+    assert data['grade'] == 'A'
+    assert data['state'] == 'GRADED'
